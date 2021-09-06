0dba98798ea6da9aaace8ddaaf2ed2a05f81cd1b
catarina <catarinapdm@gmail.com>
2021-09-06 17:44:17 +0100

    added qrcode library

--- Integrator/Job.cs
+++ Integrator/Job.cs
@@ -3,6 +3,7 @@ using System;
 using Primavera.Integration.Services.DocumentoInterno;
 using Primavera.Integration.Services.Models.DocumentoInterno;
 using System.Collections.Generic;
+using QRCode.Reader;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
@@ -10,47 +11,56 @@ namespace Integrator
 {
     public class Job
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         private readonly string PRIMAVERA_EMPRESA;
         private readonly string PRIMAVERA_UTILIZADOR;
         private readonly string PRIMAVERA_PASSWORD;
+
         public Job()
         {
-
             PRIMAVERA_EMPRESA = Settings.Default.PriEmpresa;
             PRIMAVERA_UTILIZADOR = Settings.Default.PriUtilizador;
             PRIMAVERA_PASSWORD = Settings.Default.PriPassword;
-            
-            log.Info("jobdone: PRIMAVERA_EMPRESA- "+ PRIMAVERA_EMPRESA + " | PRIMAVERA_UTILIZADOR- "+ PRIMAVERA_UTILIZADOR + " | PRIMAVERA_PASSWORD- " + PRIMAVERA_PASSWORD);
+
+            log.Info("jobdone: PRIMAVERA_EMPRESA- " + PRIMAVERA_EMPRESA + " | PRIMAVERA_UTILIZADOR- " +
+                     PRIMAVERA_UTILIZADOR + " | PRIMAVERA_PASSWORD- " + PRIMAVERA_PASSWORD);
         }
+
         public void Start()
         {
             try
             {
                 TestDocumentoInternoService();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 log.Error(e.Message, e);
             }
-            
-            
         }
 
+        private void TestReadQrCode()
+        {
+            IQrCodeReader qrCodeReader = new QrCodeReader();
+            var result = qrCodeReader.ReadQrCode("C:\\Users\\catar\\Downloads\\transferir.png");
+        }
         private void TestDocumentoInternoService()
         {
             var service = new DocumentoInternoService(PRIMAVERA_EMPRESA, PRIMAVERA_UTILIZADOR, PRIMAVERA_PASSWORD);
 
-            var linhas = new List<LinhaDocumentoInterno> {
+            var lines = new List<LinhaDocumentoInterno>
+            {
                 new LinhaDocumentoInterno("P11", "A1", 1, 1),
                 new LinhaDocumentoInterno("P6", "A1", 1, 1),
                 new LinhaDocumentoInterno("P5", "A1", 1, 1)
             };
             //var linha = new LinhaDocumentoInterno("001", "A1", 1, 1);
 
-            service.Create(new Primavera.Integration.Services.Models.DocumentoInterno.DocumentoInterno(1, DateTime.Now, DateTime.Now, "ORC2", "2020", "",1, "DEV.PRT.0000052/2020", "C1","OBS" ,"1d45ce9f-0d77-11eb-912a-00505601071b", linhas), linhas);
-            
-            
+            service.Create(
+                new Primavera.Integration.Services.Models.DocumentoInterno.DocumentoInterno(1, DateTime.Now,
+                    DateTime.Now, "ORC2", "2020", "", 1, "DEV.PRT.0000052/2020", "C1", "OBS",
+                    "1d45ce9f-0d77-11eb-912a-00505601071b", lines), lines);
         }
     }
 }
---
+++ QRCode/QRCodeReader.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using QRCodeDecoderLibrary;
+
+namespace QRCode.Reader
+{
+    public interface IQrCodeReader
+    {
+        string ReadQrCode(string imagePath);
+    }
+
+    public class QrCodeReader : IQrCodeReader
+    {
+        private readonly QRDecoder decoder;
+
+        public QrCodeReader()
+        {
+            // create QR Code decoder object
+            this.decoder = new QRDecoder();
+        }
+
+        public string ReadQrCode(string imagePath)
+        {
+            // load image to bitmap
+            var qrCodeInputImage = new Bitmap(imagePath);
+                
+                
+            // decode image
+            var dataByteArray = decoder.ImageDecoder(qrCodeInputImage);
+
+            // convert binary result to text string
+            var result = QRDecoder.ByteArrayToStr(dataByteArray[0]);
+            return result;
+        }
+    }
+}
